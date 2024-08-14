d616960319bdd388b60454e30868fa32c3537589
Adrian <adrian.talonia11@gmail.com>
2024-08-13 22:43:18 -0600

    add insert at the beginning LinkedList

---
+++ src/data_structures/_02_non_primitive/_01_linear_data_structure/_02_linked_list/_01_singly_linked_list/_01_operations/_01_insert_at_the_beginning/InsertAtTheBeginning.java
@@ -0,0 +1,58 @@
+package data_structures._02_non_primitive._01_linear_data_structure._02_linked_list._01_singly_linked_list._01_operations._01_insert_at_the_beginning;
+
+public class InsertAtTheBeginning {
+    // Function to insert a new node at the beginning of the
+    // list
+    public static Node insertAtFront(Node head,
+                                     int new_data) {
+        // Create a new node with the given data
+        Node new_node = new Node(new_data);
+
+        // Make the next of the new node point to the
+        // current head
+        new_node.next = head;
+
+        // Return the new node as the new head of the list
+        return new_node;
+    }
+
+    public static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(" " + curr.data);
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Create the linked list 2->3->4->5
+        Node head = new Node(2);
+        head.next = new Node(3);
+        head.next.next = new Node(4);
+        head.next.next.next = new Node(5);
+
+        // Print the original list
+        System.out.println("Original Linked List:");
+        printList(head);
+
+        // Insert a new node at the front of the list
+        System.out.println(
+                "After inserting Nodes at the front:");
+        int data = 1;
+        head = insertAtFront(head, data);
+
+        // Print the updated list
+        printList(head);
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int new_data) {
+        data = new_data;
+        next = null;
+    }
+}
